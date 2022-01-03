c007001371a661ddd299af3a457a53f902a2f2cc
1234684 <61401186+1234684@users.noreply.github.com>
2022-01-03 20:15:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,12 +8,12 @@
  ​#​ https://github.com/P3TERX/Actions-OpenWrt 
  ​#​ File name: diy-part2.sh 
  ​#​ Description: OpenWrt DIY script part 2 (After Update feeds) 
- ​# 
+ ​#
   
  ​#​ Modify default IP 
  ​#​sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate 
  ​#​ Default theme 
- ​sed -i ​'​s/luci-theme-bootstrap/luci-theme-opentomcat/g​'​ feeds/luci/collections/luci/Makefile 
+ ​#sed -i ​'​s/luci-theme-bootstrap/luci-theme-opentomcat/g​'​ feeds/luci/collections/luci/Makefile 
  ​#​ Delete default password 
  ​#​sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings 
  ​#​ readd cpufreq for aarch64 & Change to system 
