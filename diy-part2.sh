cbf17894dbce1a984f2306c7cfcb8a584eefd349
1234684 <61401186+1234684@users.noreply.github.com>
2022-01-31 09:07:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 # 删除软件包
 rm -rf package/lean/luci-app-netdata
 rm -rf package/lean/luci-theme-argon
