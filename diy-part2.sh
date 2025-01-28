e697e11b512a77933ae7ac865002116fc2c0511b
1234684 <61401186+1234684@users.noreply.github.com>
2025-01-28 15:36:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,6 +45,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+merge_package master https://github.com/kenzok8/small package/v2raya
 
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
